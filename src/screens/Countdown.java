e2b0321936b95d862d9e5c305f6961785e71c70f
Jacob Wise <80659147+The-Crimson-V@users.noreply.github.com>
2025-04-08 15:14:03 -0500

    Update Countdown.java to play soundtrack
    Added a method for playing a random track, which syncs with the countdown.

--- src/screens/Countdown.java
+++ src/screens/Countdown.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
+import javazoom.jl.player.Player;
+import java.io.FileInputStream;
 
 public class Countdown {
 
@@ -23,9 +25,14 @@ public class Countdown {
     public static void run(CardLayout cardLayout, JPanel cardPanel) {
         Timer timer = new Timer(1000, new ActionListener() {
             private int timeLeft = 29;
+            int trackNumber = (int) (Math.random() * 8 + 1); // Chooses random music track between Track01 and Track08
 
             @Override
             public void actionPerformed(ActionEvent evt) {
+                // Play selected soundtrack at specific time to sync with the countdown
+                if (timeLeft == 18) {
+                    startSoundtrack(trackNumber);
+                }
                 if (timeLeft >= 0) {
                     paintImage(timeLeft);
 
@@ -40,6 +47,44 @@ public class Countdown {
 
         timer.setRepeats(true);
         timer.start();
+    }
+
+    private static void startSoundtrack(int trackNumber) {
+        // Method for playing soundtrack based on the provided track number
+        String filePath = "src/audio/Track0" + trackNumber + ".mp3";
+        new Thread(() -> {
+            try {
+                // Sync tracks to say "begin" when countdown hits 0
+                if (trackNumber == 1)
+                    Thread.sleep(1633);
+                else if (trackNumber == 2)
+                    Thread.sleep(167);
+                else if (trackNumber == 3)
+                    Thread.sleep(1167);
+                else if (trackNumber == 4 || trackNumber == 5)
+                    Thread.sleep(933);
+                else if (trackNumber == 6)
+                    Thread.sleep(1767);
+                else if (trackNumber == 7)
+                    Thread.sleep(700);
+                else if (trackNumber == 8)
+                    Thread.sleep(500);
+                else
+                    System.out.println("Invalid Track Number");
+
+                try (FileInputStream track = new FileInputStream(filePath)) {
+                    Player playMP3 = new Player(track);
+                    playMP3.play();
+                }
+
+            } catch (InterruptedException e) {
+                System.err.println("Soundtrack playback was interrupted.");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.err.println("An error occurred while playing the soundtrack.");
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     private static void paintImage(Integer id) {
