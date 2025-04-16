a223305a01a27c38ace7e094699b5555fc4e36ef
Marcus Porter <map051@uark.edu>
2025-04-16 12:58:35 -0500

    Update Countdown.java

--- src/screens/Countdown.java
+++ src/screens/Countdown.java
@@ -1,21 +1,37 @@
 package screens;
 
+import udp.UDPClient;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
+import javazoom.jl.player.Player;
+import java.io.FileInputStream;
 
 public class Countdown {
 
+    static UDPClient udpClient;
+
     static JPanel panel;
+
+    public Countdown(UDPClient udp) {
+        udpClient = udp;
+    }
 
     public static JPanel init() {
         panel = new JPanel(new GridLayout());
-        panel.setPreferredSize(new Dimension(600, 300));
+        panel.setPreferredSize(new Dimension(1200, 600));
         panel.setBackground(Color.BLACK);
 
-        paintImage(30);
+        // panel is not sized at initialization. we need to wait a little
+        panel.addComponentListener(new java.awt.event.ComponentAdapter() {
+            @Override
+            public void componentResized(java.awt.event.ComponentEvent e) {
+                paintImage(30);
+            }
+        });
 
         return panel;
     }
@@ -23,15 +39,23 @@ public class Countdown {
     public static void run(CardLayout cardLayout, JPanel cardPanel) {
         Timer timer = new Timer(1000, new ActionListener() {
             private int timeLeft = 29;
+            int trackNumber = (int) (Math.random() * 8 + 1); // Chooses random music track between Track01 and Track08
 
             @Override
             public void actionPerformed(ActionEvent evt) {
+                // Play selected soundtrack at specific time to sync with the countdown
+                if (timeLeft == 18) {
+                    startSoundtrack(trackNumber);
+                }
                 if (timeLeft >= 0) {
                     paintImage(timeLeft);
 
                     timeLeft--;
                 } else {
                     ((Timer) evt.getSource()).stop();
+
+                    udpClient.transitStatusCode(202);
+                    GameAction.run();
 
                     cardLayout.show(cardPanel, "game-action");
                 }
@@ -42,14 +66,79 @@ public class Countdown {
         timer.start();
     }
 
-    private static void paintImage(Integer id) {
-        ImageIcon countdownImage = new ImageIcon(Objects.requireNonNull(Countdown.class.getClassLoader().getResource(String.format("images/countdown/%s.png", id))));
-        JLabel label = new JLabel(countdownImage, SwingConstants.CENTER);
+    private static void startSoundtrack(int trackNumber) {
+        // Method for playing soundtrack based on the provided track number
+        String filePath = "src/audio/Track0" + trackNumber + ".mp3";
+        new Thread(() -> {
+            try {
+                // Sync tracks to say "begin" when countdown hits 0
+                if (trackNumber == 1)
+                    Thread.sleep(1633);
+                else if (trackNumber == 2)
+                    Thread.sleep(167);
+                else if (trackNumber == 3)
+                    Thread.sleep(1167);
+                else if (trackNumber == 4 || trackNumber == 5)
+                    Thread.sleep(933);
+                else if (trackNumber == 6)
+                    Thread.sleep(1767);
+                else if (trackNumber == 7)
+                    Thread.sleep(700);
+                else if (trackNumber == 8)
+                    Thread.sleep(500);
+                else
+                    System.out.println("Invalid Track Number");
 
-        panel.setSize(countdownImage.getIconWidth(), countdownImage.getIconHeight());
+                try (FileInputStream track = new FileInputStream(filePath)) {
+                    Player playMP3 = new Player(track);
+                    playMP3.play();
+                }
+
+            } catch (InterruptedException e) {
+                System.err.println("Soundtrack playback was interrupted.");
+                e.printStackTrace();
+            } catch (Exception e) {
+                System.err.println("An error occurred while playing the soundtrack.");
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private static void paintImage(Integer id) {
+        ImageIcon ogBackgroundImage = new ImageIcon(Objects.requireNonNull(
+                Countdown.class.getClassLoader().getResource("images/countdown/background.png"))
+        );
+        ImageIcon ogCountdownImage = new ImageIcon(Objects.requireNonNull(
+                Countdown.class.getClassLoader().getResource(String.format("images/countdown/%s.png", id)))
+        );
+
+        ImageIcon backgroundImage = new ImageIcon(ogBackgroundImage.getImage().getScaledInstance(636, 483, Image.SCALE_SMOOTH));
+        ImageIcon countdownImage = new ImageIcon(ogCountdownImage.getImage().getScaledInstance(264, 121, Image.SCALE_SMOOTH));
+
+        JLayeredPane layeredPane = getLayeredPane(backgroundImage, countdownImage);
+
         panel.removeAll();
-        panel.add(label);
+        panel.setLayout(new BorderLayout());
+        panel.add(layeredPane, BorderLayout.CENTER);
         panel.revalidate();
         panel.repaint();
+    }
+
+    private static JLayeredPane getLayeredPane(ImageIcon backgroundImage, ImageIcon countdownImage) {
+        JLabel backgroundLabel = new JLabel(backgroundImage);
+        backgroundLabel.setBounds(0, 0, panel.getWidth(), panel.getHeight());
+
+        JLabel countdownLabel = new JLabel(countdownImage);
+        int cw = countdownImage.getIconWidth();
+        int ch = countdownImage.getIconHeight();
+
+        countdownLabel.setBounds(589, 420, cw, ch);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(panel.getSize());
+
+        layeredPane.add(backgroundLabel, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(countdownLabel, JLayeredPane.PALETTE_LAYER);
+        return layeredPane;
     }
 }
