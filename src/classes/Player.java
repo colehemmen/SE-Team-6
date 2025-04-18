5d547ed1f52817e7830c34075c2c8256f920284f
Marcus Porter <map051@uark.edu>
2025-04-18 00:05:24 -0500

    Stylized B stays forever after base hit

--- src/classes/Player.java
+++ src/classes/Player.java
@@ -1,12 +1,16 @@
 package classes;
 
 public class Player {
-    private final String codename;
+    private String codename;
     private final int score;
 
     public Player(String id, int score) {
         this.codename = id;
         this.score = score;
+    }
+
+    public void setCodename(String addB) {
+        this.codename = addB;
     }
 
     public String getCodename() {
